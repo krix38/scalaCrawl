ad099d3706b21a4588b72c991d4a95a3d1d82f44
Krzysztof Kąkol <krix38@gmail.com>
2016-06-03 08:24:00 +0200

    Refactoring

--- src/main/scala/pl/krix/scalacrawl/Crawler.scala
+++ src/main/scala/pl/krix/scalacrawl/Crawler.scala
@@ -7,26 +7,26 @@ import scala.collection.JavaConversions._
 
 object Crawler {
 
-  def getDomain(URL: String): Option[String] = {      // get domain from URL method
+  def getDomain(URL: String): Option[String] = {               // get domain from URL method
     new URI(URL).getHost match {                               // get URI's host
       case s: String => Some(s.stripPrefix("www."))            // if got string, strip useless prefix
       case null => None                                        // if got null, return none
     }
   }
 
-  def crawl(URL: String, visited: Set[String], interval: Int) {    // crawling method
-    Thread.sleep(interval)
-    val links = Jsoup.connect(URL)                             // connect
-      .get()                                                   // get content
-      .select("a[href]")                                       // get href elements from content (links)
-      .map(_.attr("abs:href"))                                 // get their absolute path
-      .filter(!_.isEmpty())                                    // weed out empty ones
-      .filter(getDomain(_) == getDomain(URL))                  // we want links from same domain only
-      .filter(!visited.contains(_))                            // we want unvisited links
-      .foreach {                                               // for every such link
-        link:String => {                                       // execute lambda which
-          println(URL + " --> " + link)                          // prints URL and its link
-          crawl(link, visited + URL, interval)                 // crawl inside link
+  def crawl(URL: String, visited: Set[String], interval: Int) {   // crawling method
+    Thread.sleep(interval)                                        // sleep before launching a request
+    Jsoup.connect(URL)                                            // connect
+      .get()                                                      // get content
+      .select("a[href]")                                          // get href elements from content (links)
+      .map(_.attr("abs:href"))                                    // get their absolute path
+      .filter(!_.isEmpty())                                       // weed out empty ones
+      .filter(getDomain(_) == getDomain(URL))                     // we want links from same domain only
+      .filter(!visited.contains(_))                               // we want unvisited links
+      .foreach {                                                  // for every such link
+        link:String => {                                          // execute lambda which
+          println(URL + " --> " + link)                           // prints URL and its link
+          crawl(link, visited + URL, interval)                    // crawl inside link
         }
       }
   }
